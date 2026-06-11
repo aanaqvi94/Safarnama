8fe28fcf8d0e561079d287bc530a34434c00558f
Asad Naqvi <aaj.naqvi@gmail.com>
2026-06-11 07:43:50 -0400

    learn: Conditionals (if, elif, else, or, and, bool, match)

---
+++ week-01/parity.py
@@ -0,0 +1,27 @@
+# Learning about modulo operator
+"""
+x = int(input("What's x? "))
+
+if x % 2 == 0:
+    print("even")
+else:
+    print("odd")
+"""
+
+def main():
+    x = int(input("What is x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
+"""
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+"""
+def is_even(n):
+    return n % 2 == 0
+
+main()
